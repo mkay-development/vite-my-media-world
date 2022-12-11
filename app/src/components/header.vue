a035d2cbf3658830239e83504d2d82bbfb6b45ea
Martz <github@jmartz.de>
2022-12-11 13:15:28 +0100

    fix mobile nav content, add login logout button

--- app/src/components/header.vue
+++ app/src/components/header.vue
@@ -22,11 +22,24 @@
       </ul>
     </nav>
   </div>
-  <div class="w-full px-2 py-2 mt-3 text-center text-black bg-gray-400 vc-nav-content md:hidden" :class="{'hidden': open}">
+  <div
+    class="w-full px-2 py-2 mt-3 text-center text-black bg-gray-400 vc-nav-content md:hidden"
+    :class="{ hidden: !open }"
+  >
     <div class="grid grid-cols-6">
-      <div class="col-span-2"><router-link to="/rack">Schrank</router-link></div>
-      <div class="col-span-2"><router-link to="/search">Suche</router-link></div>
-      <div class="col-span-2"><router-link to="/wishlist">Wunschliste</router-link></div>
+      <div class="col-span-2">
+        <router-link to="/rack">Schrank</router-link>
+      </div>
+      <div class="col-span-2">
+        <router-link to="/search">Suche</router-link>
+      </div>
+      <div class="col-span-2">
+        <router-link to="/wishlist">Wunschliste</router-link>
+      </div>
+      <div class="col-span-2">
+        <router-link v-if="loggedin" to="/login">Login</router-link>
+        <router-link v-else to="/logout">Logout</router-link>
+      </div>
     </div>
   </div>
 </template>
